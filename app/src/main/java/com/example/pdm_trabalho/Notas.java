3a483d7a7d4099d8add5bd5cfa33bcc822721221
Roberto Nunes <61832926+robnunes0727@users.noreply.github.com>
2020-09-09 13:17:33 -0300

    Initial commit

---
+++ app/src/main/java/com/example/pdm_trabalho/Notas.java
@@ -0,0 +1,14 @@
+package com.example.pdm_trabalho;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Notas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notas);
+    }
+}
---
+++ app/src/main/java/com/example/pdm_trabalho/NotasResultado.java
@@ -0,0 +1,14 @@
+package com.example.pdm_trabalho;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NotasResultado extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notas_aprovado);
+    }
+}
