d8ef51f3b3839b34513e8ec4ab2b41a7ed8c5df4
Roberto Nunes <61832926+robnunes0727@users.noreply.github.com>
2020-09-09 14:02:20 -0300

    Comentarios com as descrições de cada página

--- app/src/main/java/com/example/pdm_trabalho/Notas.java
+++ app/src/main/java/com/example/pdm_trabalho/Notas.java
@@ -8,6 +8,8 @@ public class Notas extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Calculadora de notas com 2 e envia pro NotasResultado (DA PRA FAZER)
+        // Então envia um bundle com nota + disciplina pra NotasResultado ou NotasAF
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notas);
     }
