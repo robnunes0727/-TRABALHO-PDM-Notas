e44c6aae9ee2a45a58b9cb1c2fe561bebbee1c7e
Roberto Nunes <61832926+robnunes0727@users.noreply.github.com>
2020-09-11 07:55:22 -0300

    Arquivo de créditos

---
+++ app/src/main/java/com/example/pdm_trabalho/Creditos.java
@@ -0,0 +1,15 @@
+package com.example.pdm_trabalho;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Creditos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // Activity com os nomes do grupo e link para o código
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creditos);
+    }
+}
