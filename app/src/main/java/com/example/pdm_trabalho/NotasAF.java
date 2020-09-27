1007aea5222012a6627909e9d04ac47e3b2992cb
G-besinger <greyce.kinha@gmail.com>
2020-09-27 03:47:58 -0300

    Adicionando layout

--- app/src/main/java/com/example/pdm_trabalho/NotasAF.java
+++ app/src/main/java/com/example/pdm_trabalho/NotasAF.java
@@ -3,9 +3,14 @@ package com.example.pdm_trabalho;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class NotasAF extends AppCompatActivity {
 
+    Spinner activity_notas_reprovados_spinner_Diciplinas;
     // Calculadora de notas com 3 (DA PRA FAZER VISUAL)
     // Menor nota é substituida pela nota da AF (DA PRA FAZER)
 
@@ -13,5 +18,33 @@ public class NotasAF extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notas_reprovado);
+
+        activity_notas_reprovados_spinner_Diciplinas = findViewById(R.id.spinnerDiciplinas);
+
+
+        final EditText edtNotaA1_notas = (EditText) findViewById(R.id.edtNotaA1_notas);
+        final EditText edtNotaA2_notas = (EditText) findViewById(R.id.edtNotaA2_notas);
+        final EditText edtNotaAF_notas = (EditText) findViewById(R.id.edtNotaAF_notas);
+        Button btnCalcular = (Button) findViewById(R.id.btnCancelar);
+
+        btnCalcular.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //String disciplina =
+                Float notaA1 = Float.parseFloat(edtNotaA1_notas.getText().toString());
+                Float notaA2 = Float.parseFloat(edtNotaA2_notas.getText().toString());
+                Float notaAF = Float.parseFloat(edtNotaAF_notas.getText().toString());
+
+                // Calcular as notas = notaA1+notaA2 || notaA1+notaAF || notaA2+notaAF;
+                Float notaFinal= notaA1+notaA2;
+                //Float notaFinal = if (notaA1<notaA2){
+                // notaA2+notaAF} else{
+                // notaA1+notaAF};
+
+                //Retorno na tela
+               // txtNotaFinal.setText(notaFinal.toString());
+
+            }
+        }));
     }
 }
