fc0b3071bd9921c717582893674eb01b152a2643
Roberto Nunes <61832926+robnunes0727@users.noreply.github.com>
2020-09-28 18:52:14 -0300

    Descrição do código de notasAF

--- app/src/main/java/com/example/pdm_trabalho/NotasAF.java
+++ app/src/main/java/com/example/pdm_trabalho/NotasAF.java
@@ -12,5 +12,19 @@ public class NotasAF extends AppCompatActivity {
         // Menor nota é substituida pela nota da AF (DA PRA FAZER COM JAVA)
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notas_reprovado);
+
+        /* Conta para nota:
+        notaFinal = (vem do bundle)
+        notaA1AF = notaA1 + notaAF
+        notaA2AF = notaAF + notaA1
+
+        depois testar com ifs:
+        notaFinal é maior igual que A1AF e A2AF?
+        else notaA1AF é maior igual que notaFinal e A2AF?
+        else notaA2AF é maior igual que notaFinal e A1AF?
+
+        então, refazer o pacote com as novas notas (e afBool true) e mandar para NotasResultado
+         */
+
     }
 }
