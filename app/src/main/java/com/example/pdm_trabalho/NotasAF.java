9f9ff3555d34bdbecd75fcca8b818d1678ee875a
G-besinger <greyce.kinha@gmail.com>
2020-09-28 22:03:08 -0300

    Adicionando layout

--- app/src/main/java/com/example/pdm_trabalho/NotasAF.java
+++ app/src/main/java/com/example/pdm_trabalho/NotasAF.java
@@ -2,8 +2,13 @@ package com.example.pdm_trabalho;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class NotasAF extends AppCompatActivity {
 
@@ -13,6 +18,37 @@ public class NotasAF extends AppCompatActivity {
         // Menor nota é substituida pela nota da AF (DA PRA FAZER COM JAVA)
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notas_reprovado);
+
+        final EditText edtNotaA1 = (EditText) findViewById(R.id.edtNotaA1_notas);
+        final EditText edtNotaA2 = (EditText) findViewById(R.id.edtNotaA2_notas);
+        final EditText edtNotaAF = (EditText) findViewById(R.id.edtNotaAF_notas);
+        final TextView txtNotaFinal = (TextView) findViewById(R.id.txtNotaFinal);
+        final TextView txtSituacao = (TextView) findViewById(R.id.txtSituacao);
+        Button btnCalcular = (Button) findViewById(R.id.btnCalcular);
+
+        btnCalcular.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("ResourceAsColor")
+            public void onClick(View v) {
+                // Pegando o valor digitado nos campos EditText
+                Double notaA1 = Double.parseDouble(edtNotaA1.getText().toString());
+                Double notaA2 = Double.parseDouble(edtNotaA2.getText().toString());
+
+                // Calculo a nota final
+                Double notaFinal = notaA1 + notaA2;
+                Double notaA1AF = notaA1 + notaAF;
+                Double notaA2AF = notaAF + notaA1;
+
+                // Atualizando a tela
+                txtNotaFinal.setText(notaFinal.toString());
+                if (notaFinal >= notaA1AF e notaA2AF) {
+                    txtSituacao.setText("Aprovado");
+                    txtSituacao.setTextColor(getColor(R.color.colorAprovado));
+                } else (notaA1AF >= notaFinal e notaA2AF){
+                    txtSituacao.setText("Reprovado");
+                    txtSituacao.setTextColor(getColor(R.color.colorReprovado));
+                }
+            }
+        });
 
         /* Conta para nota:
         notaFinal = (vem do bundle)
