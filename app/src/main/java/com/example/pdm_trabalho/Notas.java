1f06f6d3d425df8bddc738cdc2fd2f6f0addc926
Roberto Nunes <61832926+robnunes0727@users.noreply.github.com>
2020-10-10 11:05:58 -0300

    Finalizando "Notas"

--- app/src/main/java/com/example/pdm_trabalho/Notas.java
+++ app/src/main/java/com/example/pdm_trabalho/Notas.java
@@ -3,12 +3,20 @@ package com.example.pdm_trabalho;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Set;
 
 public class Notas extends AppCompatActivity {
 
@@ -24,31 +32,54 @@ public class Notas extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notas);
-        Toast.makeText(getApplicationContext(), "Executando onCreate!", Toast.LENGTH_LONG).show();
 
         final EditText edtNotaA1 = (EditText) findViewById(R.id.edtNotaA1);
         final EditText edtNotaA2 = (EditText) findViewById(R.id.edtNotaA2);
-        final TextView txtNotaFinal = (TextView) findViewById(R.id.txtNotaFinal);
+        final Spinner spnDisciplina = findViewById(R.id.spnDisciplina);
         Button btnCalcular = (Button) findViewById(R.id.btnCalcular);
+
+        // Puxando sharedprefs
+        SharedPreferences sharedPref = getApplicationContext().getSharedPreferences("com.example.pdm_trabalho.PREFERENCE_FILE_KEY", Context.MODE_PRIVATE);
+
+        // Coloca SharedPrefs na lista
+        Set<String> discSet = sharedPref.getStringSet("lista_de_disciplinas", null);
+
+        // Testa o conteúdo do discSet
+        if(discSet != null && !discSet.isEmpty()){
+            // Transforma set em lista e lista em adapter
+            ArrayList<String> discList = new ArrayList<String>(discSet);
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, discList);
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+            // Insere adapter no spinner
+            spnDisciplina.setAdapter(adapter);
+        } else {
+            // Se não vier nada, fechar o programa
+            Toast.makeText(this, "ERRO: Nenhuma disciplina cadastrada", Toast.LENGTH_LONG).show();
+            finish();
+        }
 
         btnCalcular.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("ResourceAsColor")
             public void onClick(View v) {
                 // Pegando o valor digitado nos campos EditText
-                Float NotaA1 = Float.parseFloat(edtNotaA1.getText().toString());
-                Float NotaA2 = Float.parseFloat(edtNotaA2.getText().toString());
+                double NotaA1 = Double.parseDouble(edtNotaA1.getText().toString());
+                double NotaA2 = Double.parseDouble(edtNotaA2.getText().toString());
 
                 // Calculo a nota final
-                Float NotaFinal = NotaA1 + NotaA2;
+                double NotaFinal = NotaA1 + NotaA2;
 
-                // Atualizando a tela
-                txtNotaFinal.setText(NotaFinal.toString());
+                // Abrindo novo intent de activity
+                Intent novaActivity = new Intent(v.getContext(), NotasResultado.class);
 
                 Bundle bundle = new Bundle();
-                bundle.putFloat("NotaA1", NotaA1);
-                bundle.putFloat("NotaA2", NotaA2);
-                bundle.putFloat("NotaFinal", NotaFinal);
-                bundle.putBoolean("AFbool", false);
+                bundle.putDouble("notaA1", NotaA1);
+                bundle.putDouble("notaA2", NotaA2);
+                bundle.putDouble("notaFinal", NotaFinal);
+                bundle.putString("disciplina", spnDisciplina.getSelectedItem().toString());
+                bundle.putBoolean("afBool", false);
+
+                startActivity(novaActivity.putExtras(bundle));
             }
         });
 } }
